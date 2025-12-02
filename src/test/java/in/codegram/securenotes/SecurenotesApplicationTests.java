c504bfd215ad070b76b7be451f1284fe0d0347e2
Pankaj Sharma <pankajsimmc@gmail.com>
2025-12-02 09:59:12 +0530

    Initial Commit for Notes App(API)

---
+++ src/test/java/in/codegram/securenotes/SecurenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codegram.securenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
